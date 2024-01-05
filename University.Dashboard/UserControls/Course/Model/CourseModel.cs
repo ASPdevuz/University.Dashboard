5a7f4fc2cfc405875d0a7241a338c74feebd2432
Abdulvosid Foziljonov <89521523+foziljonov7@users.noreply.github.com>
2024-01-05 16:39:22 +0500

    Create model

---
+++ University.Dashboard/UserControls/Course/Model/CourseModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace University.Dashboard.UserControls.Course.Model
+{
+    public class CourseModel
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public double Level { get; set; }
+        public string Duration { get; set; }
+        public string Students { get; set; }
+    }
+}
---
+++ University.Dashboard/UserControls/Student/Model/StudentModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace University.Dashboard.UserControls.Student.Model
+{
+    public class StudentModel
+    {
+        public Guid Id { get; set; }
+        public string Fullname { get; set; }
+        public string PhoneNumber { get; set; }
+        public int Age { get; set; }
+        public string Direction { get; set; }
+        public int Degree { get; set; }
+        public int FacultyId { get; set; }
+        public string CourseName { get; set; }
+    }
+}
